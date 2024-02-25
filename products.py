b8bb9a2a4dc938f869569e929b5d73eb2e972991
Lynn Chiee <lo900419@gmail.comm>
2024-02-25 20:08:58 +0800

    add write

--- products.py
+++ products.py
@@ -1,6 +1,8 @@
 products = []
 with open('products.csv', 'r', encoding='utf-8') as f:
     for line in f:
+        if '商品,價格' in line:
+            continue
         name, price = line.strip().split(',')
         products.append([name, price])
 print(products)
@@ -14,6 +16,7 @@ while True:
 print(products)
 for p in products:
     print(p[0], '的價格是', p[1])
+
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
